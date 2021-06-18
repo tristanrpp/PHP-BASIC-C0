850a40d547d2b7e04d1580ae8f4970f936eae988
Tristan <Triatan.Panggabean@student.rocva.nl>
2021-06-18 12:04:07 +0200

    php basic 2- taak08

---
+++ 2-Variabelen-en-datatype/taak08 - data en html combineren/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam1 = "Tristan";
+
+?>
+
+<?php
+$voornaam2 = "Max";
+
+?>
+
+<?php
+$voornaam3 = "Henk";
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+?>
+
+</html>
